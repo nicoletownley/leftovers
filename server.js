82de7b6f0c15ef4e764d6bae8712be0ecb8e030d
nicoletownley <natownley@gmail.com>
2019-12-07 10:16:49 -0600

    heroku take 4?

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.get('*', (request, response) => {
-	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+	response.sendFile(path.join(__dirname, 'view', 'index.html'));
 });
 
 
